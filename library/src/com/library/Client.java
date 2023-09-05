f7cfd0ea207a3603c092d2ce2c87e871b1399e16
mrImad007 <imadeddinzaoui@gmail.com>
2023-09-05 13:50:12 +0100

    working on repository structure

--- library/src/com/library/Client.java
+++
@@ -1,7 +0,0 @@
-package com.library;
-
-public class Client {
-    public static void main(String[] args) {
-
-    }
-}
--- library/src/com/library/Emprunt.java
+++
@@ -1,7 +0,0 @@
-package com.library;
-
-public class Emprunt {
-    public static void main(String[] args) {
-
-    }
-}
